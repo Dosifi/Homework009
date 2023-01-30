d28f4f1559dbb4f55d72778dfcb0ed6883c46c53
Vladislav <vpfedotoff@gmail.com>
2023-01-30 15:14:55 +0300

    added Task064

---
+++ Task064/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 64: Выполнить с помощью рекурсии.Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+Console.WriteLine("Введите натуральное значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int k = 0;
+if (n <= k)
+{
+    Console.WriteLine("Ввели не положительное число");
+}
+
+int NaturalNumbers(int n, int k)
+{
+    if (n == k)
+        return n;
+    else
+        Console.Write($"{NaturalNumbers(n, k + 1)} ");
+    return k;
+}
+
+NaturalNumbers(n, k);
